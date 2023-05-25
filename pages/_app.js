8d65fb0538a26a2b9bec674cb7db74ee33003329
Ayo <heyayomideadebisi@gmail.com>
2023-05-25 23:41:22 +0100

    configure next auth context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps: { session, ...pageProps }, }) {
   return (
     <>
       <Head>
@@ -10,7 +11,9 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   )
 }
