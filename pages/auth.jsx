6904cf8e48eaddef950e87918af094b7a2524c39
Ayo <heyayomideadebisi@gmail.com>
2023-05-25 23:47:41 +0100

    add auth content

---
+++ pages/auth.jsx
@@ -0,0 +1,11 @@
+import {signIn} from "next-auth/react"
+const AuthPage = () => {
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+        <button>sign in with google</button>
+    </div>
+  )
+}
+
+export default AuthPage
