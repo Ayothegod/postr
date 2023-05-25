2f3fb539bb3ce6422b79c7aade125ae9347821a7
Ayo <heyayomideadebisi@gmail.com>
2023-05-25 23:50:13 +0100

    finish styling auth

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -3,7 +3,7 @@ const AuthPage = () => {
 
   return (
     <div className="flex items-center justify-center h-screen">
-        <button>sign in with google</button>
+        <button className="bg-gray-800 text-white py-2 px-6 rounded hover:bg-gray-900">sign in with google</button>
     </div>
   )
 }
