764ea79e831d42349aceb1d177436b6654f118af
Ayo <heyayomideadebisi@gmail.com>
2023-05-26 00:56:06 +0100

    built auth page

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -1,15 +1,12 @@
-import { signIn, signOut, useSession } from "next-auth/react"
+import { signIn, } from "next-auth/react"
 import toast from 'react-hot-toast';
 
 
 const AuthPage = () => {
-
     const login = () => {
         signIn('google', { callbackUrl: '/' })
         toast.success("Lets goo")
     }
-
-
     return (
         <div className="flex items-center justify-center h-screen">
             <button className="bg-gray-800 text-white py-2 px-6 rounded hover:bg-gray-900" onClick={login}>sign in with google</button>
