cdc62ba4a6f2e9c6c99c5a8478ee12bfbfc2d3a0
Ayo <heyayomideadebisi@gmail.com>
2023-05-25 23:53:20 +0100

    add hot toast

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -1,9 +1,11 @@
-import {signIn} from "next-auth/react"
+import {signIn, useSession} from "next-auth/react"
 const AuthPage = () => {
-
+  const session = useSession()
+  console.log(session);
   return (
     <div className="flex items-center justify-center h-screen">
-        <button className="bg-gray-800 text-white py-2 px-6 rounded hover:bg-gray-900">sign in with google</button>
+        <button className="bg-gray-800 text-white py-2 px-6 rounded hover:bg-gray-900" onClick={() => signIn("google")}>sign in with google</button>
+        <button className="bgwhite text-black py-2 px-6 rounded" onClick={() => signIn("google")}>sign in with google</button>
     </div>
   )
 }
