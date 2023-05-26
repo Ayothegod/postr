df2826c65348437323981730c957f5e7b36c22f5
Ayo <heyayomideadebisi@gmail.com>
2023-05-26 09:43:20 +0100

    initial create post design

--- components/CreatePosts.jsx
+++ components/CreatePosts.jsx
@@ -2,7 +2,13 @@
 
 const CreatePosts = () => {
   return (
-    <div>CreatePosts</div>
+    <div className="p-4 bg-white rounded">
+        <textarea rows="3" className="bg-gray-300 w-full rounded p-2" placeholder="what is on your mind?"/>
+        <div className="flex items-center justify-between mt-2">
+            <p>300</p>
+            <button className="bg-blue-500 text-white text-sm rounded py-1 px-6 font-semibold">create post</button>
+        </div>
+    </div>
   )
 }
 
