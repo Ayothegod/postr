80128eb43ecb1e017a0a606949e86143a107be17
Ayo <heyayomideadebisi@gmail.com>
2023-05-26 01:11:22 +0100

    add auth image to next config domain

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps: { session, ...pageProps }, }
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <SessionProvider session={session}>
+
         <Toaster />
         <Component {...pageProps} />
       </SessionProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 // import { Inter } from 'next/font/google'
 import {useSession} from "next-auth/react"
+import Image from "next/image";
 import Link from "next/link";
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -8,14 +9,23 @@ export default function Home() {
   console.log(session);
   return (
     <>
-      <main className="max-w-[40rem] mx-auto">
+        <div className='bg-gray-300 min-h-screen'>
+
+      <main className="max-w-[40rem] mx-auto ">
         <div className="mx-4">
-          <nav className="flex justify-between">
+          <nav className="flex justify-between py-4">
             <Link href="/">Postr</Link>
-            <Link href="/">Postr</Link>
+
+            {session ? 
+            <Link href="/dashboard">
+              <Image src={session?.data.user.image} alt={session?.data.user.name} width={24} height={24}/>
+            </Link> : 
+            <button className="bg-gray-800 text-white py-2 px-6 rounded">sign in</button>
+            }
           </nav>
         </div>
       </main>
+    </div>
     </>
   )
 }
