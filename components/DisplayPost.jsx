a58e2ce4cea8bd6a6c19f2e3bcea64e63abd1092
Ayo <heyayomideadebisi@gmail.com>
2023-05-26 13:27:35 +0100

    useSwr

--- components/DisplayPost.jsx
+++ components/DisplayPost.jsx
@@ -1,6 +1,11 @@
 import axios from 'axios'
+import useSWR from 'swr'
 
 const DisplayPost = () => {
+    const fetcher = async(url) => {
+        await axios.get(url).then(res => res.data)
+    }
+
   return (
     <div>DisplayPost</div>
   )
