001a2b085a0f301660f841a0744428db5be74050
Ayo <heyayomideadebisi@gmail.com>
2023-05-26 23:59:43 +0100

    git fetched after repo name change

--- pages/api/post/getAllPosts.js
+++ pages/api/post/getAllPosts.js
@@ -8,7 +8,6 @@ import prisma from '@/lib/prisma';
 
 export default async function handler(req, res) {
     await NextCors(req, res, {
-        // Options
         methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
         origin: '*',
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
