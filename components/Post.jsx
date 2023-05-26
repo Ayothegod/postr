a3f4ef51281d30210b8258147a293c91c7f89dff
Ayo <heyayomideadebisi@gmail.com>
2023-05-26 15:50:51 +0100

    adding comments schema

--- components/Post.jsx
+++ components/Post.jsx
@@ -4,13 +4,16 @@ import Image from "next/image"
 const Post = ({postData}) => {
     console.log(postData)
   return (
-    <div className="flex flex-col gap-2 bg-white p-2 my-1 rounded">
+    <div className="flex flex-col gap-1 bg-white p-2 my-1 rounded">
         <div className="flex items-center gap-2">
             <Image src={postData.user.image} alt={postData.user.name} width={36} height={36} className="rounded-full"/>
             <p className="text-sm font-medium">{postData.user.name}</p>
         </div>
         <div>
-            <p className="pl-4 text-sm text-gray-600">{postData.post}</p>
+            <p className=" text-gray-600">{postData.post}</p>
+        </div>
+        <div>
+            <p className=" text-sm text-gray-600">comments</p>
         </div>
     </div>
   )
