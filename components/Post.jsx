37df9fd2b9bb50b906d7725153ee1abd383e0ce0
Ayo <heyayomideadebisi@gmail.com>
2023-05-26 17:14:49 +0100

    time to build single post page

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image"
+import Link from "next/link";
 
 
 const Post = ({postData}) => {
   console.log(postData);
   return (
-    <div className="flex flex-col gap-1 bg-white p-2 my-1 rounded">
+    <Link href={`/${postData.id}`} className="flex flex-col gap-1 bg-white p-2 my-1 rounded">
         <div className="flex items-center gap-2">
             <Image src={postData.user.image} alt={postData.user.name} width={36} height={36} className="rounded-full"/>
             <p className="text-sm font-semibold">{postData.user.name}</p>
@@ -13,9 +14,9 @@ const Post = ({postData}) => {
             <p className=" text-gray-800 ">{postData.post}</p>
         </div>
         <div>
-            <p className=" text-sm text-gray-600">comments</p>
+            <p className=" text-sm text-gray-600">{postData.comments.length} comments</p>
         </div>
-    </div>
+    </Link>
   )
 }
 
