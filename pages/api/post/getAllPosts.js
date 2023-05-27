20c6fee534078487b8accd970c2733ee300e4630
Ayo <heyayomideadebisi@gmail.com>
2023-05-27 11:52:08 +0100

    add cors again

--- pages/api/post/getAllPosts.js
+++ pages/api/post/getAllPosts.js
@@ -1,12 +1,12 @@
-// import NextCors from 'nextjs-cors';
+import NextCors from 'nextjs-cors';
 import prisma from '@/lib/prisma';
 
 export default async function handler(req, res) {
-    // await NextCors(req, res, {
-    //     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    //     origin: '*',
-    //     optionsSuccessStatus: 200,
-    //  });
+    await NextCors(req, res, {
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200,
+     });
     if (req.method === "GET") {
         try {
             const allPosts = await prisma.Post.findMany({
