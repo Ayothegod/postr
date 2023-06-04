e53f5bf933ed9195445801af1bb38da99498b78f
Ayo <heyayomideadebisi@gmail.com>
2023-06-04 15:32:06 +0100

    fetch ind posta with comments

--- pages/api/post/getUserPosts.js
+++ pages/api/post/getUserPosts.js
@@ -1,5 +1,7 @@
 import NextCors from 'nextjs-cors';
 import prisma from '@/lib/prisma';
+import { authOptions } from 'pages/api/auth/[...nextauth]'
+import { getServerSession } from "next-auth/next"
 
 export default async function handler(req, res) {
     await NextCors(req, res, {
@@ -8,9 +10,18 @@ export default async function handler(req, res) {
         optionsSuccessStatus: 200,
      });
     if (req.method === "GET") {
+        const session = await getServerSession(req, res, authOptions)
+        if (!session) return res.status(403).json("You need to login")
         try {
+            const user = await prisma.User.findUnique({
+                where:{
+                    email:session.user.email
+                }
+            })
             const allPosts = await prisma.Post.findMany({
-
+                where:{
+                    userId:user.id
+                },
                 include: {
                     user: true,
                     comments: true
@@ -19,6 +30,7 @@ export default async function handler(req, res) {
                     createdAt: "desc"
                 }
             })
+            // console.log(user);
             // console.log(allPosts);
             res.status(201).json({allPosts})
         } catch (error) {
--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,3 +1,4 @@
+import DisplayPost from "@/components/DisplayPost"
 import Header from "@/components/Header"
 
 
@@ -6,6 +7,7 @@ const Dashboard = () => {
     <div className='bg-gray-300 min-h-screen'>
         <main className="mx-4 sm:mx-auto max-w-[40rem]">
           <Header/>
+          <DisplayPost/>
         </main>
       </div>
   )
