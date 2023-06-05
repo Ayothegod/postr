db3db35b9bfa6fe96e66883f3cede9a9ac4772c1
Ayo <heyayomideadebisi@gmail.com>
2023-06-05 17:13:09 +0100

    like is still somehow

--- components/Post.jsx
+++ components/Post.jsx
@@ -3,10 +3,12 @@ import Image from "next/image"
 import Link from "next/link";
 import { useState } from "react";
 import ModalOff from "./ModalOff";
-import {AiOutlineHeart} from "react-icons/ai"
+import { AiFillHeart } from "react-icons/ai"
 
 const Post = ({ postData, dashboard }) => {
+  console.log(postData);
   const [deleteModal, setDeleteModal] = useState(false)
+  const [clickLike, setClickLike] = useState(false)
   return (
     <div className="flex flex-col gap-1 bg-white px-4 py-2 my-1 rounded ">
       <Link href={`/${postData.id}`}>
@@ -21,20 +23,24 @@ const Post = ({ postData, dashboard }) => {
 
         </div>
       </Link>
-          <div className="border-t border-dotted border-gray-200">
-            <div className="flex gap-12 items-center ">
-              <p className=" text-sm text-gray-600">likes</p>
-            <p className=" text-sm text-gray-600">{postData?.comments.length} comments</p>
-            {
-              dashboard &&
-              <button className="text-red-600 font-bold text-base" onClick={() => setDeleteModal(!deleteModal)}>delete</button>
-            }
-            {
-              deleteModal &&
-              <ModalOff setDeleteModal={setDeleteModal} id={postData.id} deleteModal={deleteModal} />
-            }
-            </div>
-          </div>
+      <div className="border-t border-dotted border-gray-200 py-1">
+        <div className="flex gap-12 items-center ">
+          <p className=" text-sm text-gray-600 flex items-center gap-1" onClick={() => setClickLike(!clickLike)}>
+            <AiFillHeart className={`text-xl ${clickLike && "text-red-600"}`}/>
+            {postData.likes.length}
+            {/* like */}
+          </p>
+          <p className=" text-sm text-gray-600">{postData?.comments.length} comments</p>
+          {
+            dashboard &&
+            <button className="text-red-600 font-bold text-base" onClick={() => setDeleteModal(!deleteModal)}>delete</button>
+          }
+          {
+            deleteModal &&
+            <ModalOff setDeleteModal={setDeleteModal} id={postData.id} deleteModal={deleteModal} />
+          }
+        </div>
+      </div>
     </div>
   )
 }
